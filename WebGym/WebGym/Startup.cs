6e2d58adc828f6c88ab74bedcabf7b5e2b0ad597
Coercivity <digitalpirouette@gmail.com>
2022-04-22 18:15:14 +0300

    Create connection string

--- WebGym/WebGym/Startup.cs
+++ WebGym/WebGym/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,9 +15,17 @@ namespace WebGym
     public class Startup
     {
 
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.AddDbContext<GymContext>(op => op.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
 
